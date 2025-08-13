52332bb13a00bc9c883989ce449836f6e3d73a64
Aleksander Baska <baskaaleksander03@gmail.com>
2025-08-13 23:15:38 +0200

    fix: update project titles for clarity and consistency

--- data/projects.ts
+++ data/projects.ts
@@ -1,18 +1,18 @@
 export const projects = [
-	{
-		title: 'E-Book Marketplace',
-		description:
-			'Modern, full-stack eBook marketplace that connects readers with authors and publishers. The platform provides a seamless experience for buying, selling, and discovering digital books across multiple genres and categories.',
-		tech: ['Next.js', 'AWS S3', 'Stripe', 'PostgreSQL', 'Docker'],
-		link: 'https://ebook-marketplace-chi.vercel.app/',
-		githubLink: 'https://github.com/baskaaleksander/ebook-marketplace',
-	},
-	{
-		title: 'Financial Tracker API',
-		description:
-			'A RESTful API for personal financial management built with Node.js, Express, TypeScript, and MongoDB. This application allows users to track their income and expenses, categorize transactions, and generate financial reports.',
-		tech: ['Express.js', 'TypeScript', 'MongoDB', 'Jest', 'Swagger/OpenAPI'],
-		link: 'https://fintracker.baskaaleksander.com/api-docs',
-		githubLink: 'https://github.com/baskaaleksander/financial-tracker-api',
-	},
+  {
+    title: "E-Book Marketplace (Bookify)",
+    description:
+      "Modern, full-stack eBook marketplace that connects readers with authors and publishers. The platform provides a seamless experience for buying, selling, and discovering digital books across multiple genres and categories.",
+    tech: ["Next.js", "AWS S3", "Stripe", "PostgreSQL", "Docker"],
+    link: "https://ebook-marketplace-chi.vercel.app/",
+    githubLink: "https://github.com/baskaaleksander/ebook-marketplace",
+  },
+  {
+    title: "Financial Tracker API",
+    description:
+      "A RESTful API for personal financial management built with Node.js, Express, TypeScript, and MongoDB. This application allows users to track their income and expenses, categorize transactions, and generate financial reports.",
+    tech: ["Express.js", "TypeScript", "MongoDB", "Jest", "Swagger/OpenAPI"],
+    link: "https://fintracker.baskaaleksander.com/api-docs",
+    githubLink: "https://github.com/baskaaleksander/financial-tracker-api",
+  },
 ];
